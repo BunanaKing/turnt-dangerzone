0b8c81df57e025e364c2958d71f8726046710af1
Diego Caceres <dado90caceres@gmail.com>
2014-09-14 15:58:17 -0300

    initial commit

---
+++ Assets/Scripts/ButtonsControl.cs
@@ -0,0 +1,81 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ButtonsControl : MonoBehaviour
+{
+    public Button[] buttons;
+    public Floor floor;
+
+    void Start()
+    {
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.LeftArrow))
+        {
+            floor.AddColor(Button.BallColor.Red);
+        }
+
+        if (Input.GetKeyDown(KeyCode.DownArrow))
+        {
+            floor.AddColor(Button.BallColor.Yellow);
+        }
+
+        if (Input.GetKeyDown(KeyCode.RightArrow))
+        {
+            floor.AddColor(Button.BallColor.Blue);
+        }
+
+        if (Input.GetKeyUp(KeyCode.LeftArrow))
+        {
+            floor.RemoveColor(Button.BallColor.Red);
+        }
+
+        if (Input.GetKeyUp(KeyCode.DownArrow))
+        {
+            floor.RemoveColor(Button.BallColor.Yellow);
+        }
+
+        if (Input.GetKeyUp(KeyCode.RightArrow))
+        {
+            floor.RemoveColor(Button.BallColor.Blue);
+        }
+
+        // Look for all fingers
+        for (int i = 0; i < Input.touchCount; i++)
+        {
+            Touch touch = Input.GetTouch(i);
+            Debug.Log("Touch");
+
+            if (touch.phase == TouchPhase.Began)
+            {
+                //Touch are screens location. Convert to world
+                Vector3 position = Camera.main.ScreenToWorldPoint(touch.position);
+
+                foreach (Button button in buttons)
+                {
+                    if (button.collider2D.OverlapPoint(position))
+                    {
+                        floor.AddColor(button.color);
+                        break;
+                    }
+                }
+            }
+            else if (touch.phase == TouchPhase.Ended)
+            {
+                //Touch are screens location. Convert to world
+                Vector3 position = Camera.main.ScreenToWorldPoint(touch.position);
+
+                foreach (Button button in buttons)
+                {
+                    if (button.collider2D.OverlapPoint(position))
+                    {
+                        floor.RemoveColor(button.color);
+                        break;
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/Timer.cs
@@ -0,0 +1,59 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Timer : MonoBehaviour
+{
+    private float startTime;
+    private float restSeconds;
+    private float roundedRestSeconds;
+    private float displaySeconds;
+    private float displayMinutes;
+    private float addedSeconds = 0;
+
+    public int countDownSeconds;
+
+
+    void Start()
+    {
+    }
+
+    public void StartTimer()
+    {
+        enabled = true;
+        startTime = Time.time;
+    }
+
+    void OnGUI()
+    {
+        //make sure that your time is based on when this script was first called
+        //instead of when your game started
+        int guiTime = (int)(Time.time - startTime);
+
+        restSeconds = countDownSeconds + addedSeconds - guiTime;
+
+        //display messages or whatever here -->do stuff based on your timer
+        if (restSeconds == 60)
+        {
+            print("One Minute Left");
+        }
+        if (restSeconds <= 0)
+        {
+            print("Time is Over");
+            enabled = false;
+            //do stuff here
+        }
+
+        //display the timer
+        roundedRestSeconds = Mathf.CeilToInt(restSeconds);
+        displaySeconds = roundedRestSeconds % 60;
+        displayMinutes = roundedRestSeconds / 60;
+
+        string text = string.Format("{0:00}:{1:00}", displayMinutes, displaySeconds);
+        GUI.Label(new Rect(20, 20, 100, 30), text);
+    }
+
+    public void Add(int seconds)
+    {
+        addedSeconds += seconds;
+    }
+}
