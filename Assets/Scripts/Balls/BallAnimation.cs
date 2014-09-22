579a032f6a03147422e6f09257b5a4afdc48d6e2
iuval <iuvalgoldansky@gmail.com>
2014-09-21 23:37:06 -0200

    Add basic ball animations

---
+++ Assets/Scripts/Balls/BallAnimation.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BallAnimation : MonoBehaviour
+{
+    private Animator animator;
+    public float sleepTime;
+
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+        sleepTime = Random.Range(5, 30);
+    }
+
+    void Update()
+    {
+        sleepTime -= Time.deltaTime;
+
+        if (sleepTime <= 0)
+        {
+            switch (Random.Range(0, 3))
+            {
+                case 0:
+                    {
+                        animator.SetTrigger("Happy");
+                        break;
+                    }
+                case 1:
+                    {
+                        animator.SetTrigger("Wow");
+                        break;
+                    }
+                case 2:
+                    {
+                        animator.SetTrigger("Wierd");
+                        break;
+                    }
+            }
+
+            sleepTime = Random.Range(5, 30);
+        }
+    }
+}
